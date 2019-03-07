5d5e29b140d0379e04bccb30998b52e18d7ed27f
jarom <jaromero.marz.234@gmail.com>
2019-03-06 18:51:37 -0700

    segundo commit

---
+++ app/Http/Controllers/NoticeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NoticeController extends Controller
+{
+    //
+    public function getCatalog(){
+
+    }
+    public function getAdd(){
+
+    }
+    public function Store(){
+        
+    }
+}
