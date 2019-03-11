98dc02e0f03fd4b77791b8f570d75c001cc8317a
jarom <jaromero.marz.234@gmail.com>
2019-03-11 12:07:10 -0700

    añadir panel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Notice;
 class HomeController extends Controller
 {
     private $user;
+    private $notices;
     /**
      * Create a new controller instance.
      *
@@ -26,5 +28,11 @@ class HomeController extends Controller
     {
         $this->user=Auth::user();
         return view('Home.home')->with("user",$this->user);
+    }
+    public function Panel(){
+        //$this->notices=Notice::where("autor",Auth::user()->name)->orderBy("fecha","desc");
+        //$this->notices=Notice::all()->where("autor","=","Jarom Mariscal Zozaya");
+        $this->notices= \DB::table('noticie')->where("autor","=",Auth::user()->name)->orderBy('fecha',"desc")->get();
+        return view('Home.panel')->with("user",$this->user)->with("lista_noticias",$this->notices);
     }
 }
